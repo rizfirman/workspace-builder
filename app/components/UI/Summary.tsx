9140369bd49ad731ec5ce6501e21b650d0ff78ad
Rizki Firmansyah <rizfirman@gmail.com>
2026-04-05 23:02:54 +0700

    add: cursor-pointer for every button

--- app/components/UI/Summary.tsx
+++ app/components/UI/Summary.tsx
@@ -10,7 +10,7 @@ export default function Summary() {
     <div className="w-105 rounded-2xl border border-white/20 bg-white/10 px-6 py-4 shadow-xl backdrop-blur-xl">
       <div className="mb-4 flex items-center justify-between">
         <h2 className="font-semibold">Your Setup</h2>
-        <button className="rounded-xl bg-white px-4 py-2 text-sm text-black transition hover:scale-105">
+        <button className="cursor-pointer rounded-xl bg-white px-4 py-2 text-sm text-black transition hover:scale-105">
           Rent Now
         </button>
       </div>
